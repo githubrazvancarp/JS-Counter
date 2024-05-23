ffedd0d3b4ba18c3eeb7e2f3880d56534cd355ba
Razvan Carp <razvan_ector@yahoo.com>
2024-05-23 17:07:00 +0300

    second

---
+++ app.js
@@ -0,0 +1,32 @@
+// set initial Count
+
+let count = 0;
+
+// select values and buttons
+
+const value = document.querySelector ("#value");
+const btns = document.querySelectorAll (".btn");
+
+btns.forEach(function (btn)  {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")) {
+            count--;
+        } 
+        else if(styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }        
+        if (count > 0) {
+            value.style.color = "blue";          
+        }
+        if (count < 0) {
+            value.style.color = "purple";            
+        }
+        if (count === 0) {
+            value.style.color = "#222";    
+        }
+        value.textContent = count;
+    });
+});
